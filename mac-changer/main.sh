ba606f8aa89720f13a19a376a8c524d5a46fe3b5
Kadir Islow <kadirislow@gmail.com>
2020-11-25 21:41:43 +0100

    slight refactor

--- mac-changer/main.sh
+++ mac-changer/main.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-GET_DEFAULT_INTERFACE=`route | grep '^default' | grep -o '[^ ]*$'`
+GET_DEFAULT_INTERFACE=$(route | grep '^default' | grep -o '[^ ]*$')
 
-GET_MAC_ADDRESS=`ip link show $GET_DEFAULT_INTERFACE | awk '/ether/ {print $2}'`
+GET_MAC_ADDRESS=$(ip link show $GET_DEFAULT_INTERFACE | awk '/ether/ {print $2}')
 
 echo "Current MAC address in use: |$GET_MAC_ADDRESS|" \
 
 if [[ ! -z $GET_MAC_ADDRESS ]];
 then
     # generate a random mac address
-    GENERATE_RANDOM_MAC=`sudo openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//'`
+    GENERATE_RANDOM_MAC=$(sudo openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')
     echo "Random MAC address: |$GENERATE_RANDOM_MAC|" \ 
     
     if [[ $GET_MAC_ADDRESS == $GENERATE_RANDOM_MAC ]];
@@ -21,12 +21,11 @@ then
         # temporarily disable interface to avoid error message
         sudo ip link set dev $GET_DEFAULT_INTERFACE down
         # apply newly generated mac address
-        sudo ip link set dev "$GET_DEFAULT_INTERFACE" address $GENERATE_RANDOM_MAC
+        sudo ip link set dev $GET_DEFAULT_INTERFACE address $GENERATE_RANDOM_MAC
         # enable interface
-        sudo ip link set dev "$GET_DEFAULT_INTERFACE" up
+        sudo ip link set dev $GET_DEFAULT_INTERFACE up
 
-        GET_NEW_INTERFACE_MAC=`ip link show $GET_DEFAULT_INTERFACE | awk '/ether/ {print $2}'`
-        
+        GET_NEW_INTERFACE_MAC=$(ip link show $GET_DEFAULT_INTERFACE | awk '/ether/ {print $2}')
         echo "Applying random MAC address |$GET_NEW_INTERFACE_MAC| to interface ($GET_DEFAULT_INTERFACE)"
     fi
 fi
